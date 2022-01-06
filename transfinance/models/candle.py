6525c0f304f0cbb2dd0d046fb48082a6a27b6f6b
erondondron <smirnov.andrey.serg@gmail.com>
2022-01-06 22:52:20 +0300

    Модель свечей реализована через pydantic

--- transfinance/models/candle.py
+++ transfinance/models/candle.py
@@ -1,46 +1,20 @@
 from datetime import datetime
 
+from pydantic import BaseModel
 from tinvest import Candle as TCandle
 
 
-class Candle:
+class Candle(BaseModel):
     """
-    Значения цен указаны в минимальных производных валюты (копейки, центы)
+    Значения цен указаны в минимальных производных валюты (копейки, центы).
+    Длительность свечи равна минуте.
     """
-
-    def __init__(
-        self, o: int, c: int, h: int, l: int, v: int, t: datetime
-    ) -> None:
-        self._open: int = o
-        self._close: int = c
-        self._high: int = h
-        self._low: int = l
-        self._value: int = v
-        self._time: datetime = t
-
-    @property
-    def open_price(self) -> int:
-        return self._open
-
-    @property
-    def close_price(self) -> int:
-        return self._close
-
-    @property
-    def high_price(self) -> int:
-        return self._high
-
-    @property
-    def low_price(self) -> int:
-        return self._low
-
-    @property
-    def value(self) -> int:
-        return self._value
-
-    @property
-    def time(self) -> datetime:
-        return self._time
+    open: int
+    close: int
+    high: int
+    low: int
+    value: int
+    time: datetime
 
     @staticmethod
     def from_tinkoff_candle(t_candle: TCandle) -> "Candle":
