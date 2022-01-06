7e7f0bb65f41d63e7016088b1cc34403e8ed11ca
erondondron <smirnov.andrey.serg@gmail.com>
2022-01-06 21:33:55 +0300

    Добавлена реализация клиента для работы с API тинькофф

---
+++ transfinance/models/client.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from typing import List
+
+from tinvest import (
+    SyncClient,
+    SandboxRegisterRequest,
+    BrokerAccountType,
+    MarketInstrument,
+    CandleResolution,
+    Candle,
+)
+
+from transfinance.settings import TINKOFF_SANDBOX_TOKEN, FIGI_FILE
+
+
+class TinkoffClient:
+    sync = SyncClient(TINKOFF_SANDBOX_TOKEN, use_sandbox=True)
+
+    @staticmethod
+    def register_broker() -> str:
+        broker_type = BrokerAccountType.tinkoff
+        req = SandboxRegisterRequest(broker_account_type=broker_type)
+        resp = TinkoffClient.sync.register_sandbox_account(req)
+        broker_id: str = resp.payload.broker_account_id
+        return broker_id
+
+    @staticmethod
+    def clear_all_brokers() -> None:
+        brokers = TinkoffClient.sync.get_accounts().payload.accounts
+        for b in brokers:
+            TinkoffClient.sync.clear_sandbox_account(b.broker_account_id)
+            TinkoffClient.sync.remove_sandbox_account(b.broker_account_id)
+
+    @staticmethod
+    def get_candles(
+        figi: str,
+        from_dt: datetime,
+        to_dt: datetime,
+        resolution: CandleResolution,
+    ) -> List[Candle]:
+        resp = TinkoffClient.sync.get_market_candles(
+            figi, from_dt, to_dt, resolution
+        )
+        candles: List[Candle] = resp.payload.candles
+        return candles
+
+    @staticmethod
+    def get_market_stocks() -> List[MarketInstrument]:
+        resp = TinkoffClient.sync.get_market_stocks()
+        stocks: List[MarketInstrument] = resp.payload.instruments
+        return stocks
+
+    @staticmethod
+    def dump_market_stocks() -> None:
+        stocks = [
+            f"{s.name} ({s.currency.value}): {s.figi}"
+            for s in TinkoffClient.get_market_stocks()
+        ]
+        with open(FIGI_FILE, "w", encoding="utf-8") as file:
+            file.write("\n".join(stocks))
