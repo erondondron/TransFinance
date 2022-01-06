abb64b1f7865214fb21bebdc121a796b7dd6f34d
erondondron <smirnov.andrey.serg@gmail.com>
2022-01-06 21:32:16 +0300

    Добавлено чтение пути к рабочей директории из конфигурационного файла

--- transfinance/settings.py
+++ transfinance/settings.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from pathlib import Path
-from typing import Dict, Any, TypeVar, Optional
+from typing import Dict, Any, TypeVar, Optional, Type
 
 import yaml
 
@@ -12,11 +12,13 @@ class ConfigFields(Enum):
     TINKOFF_MARKET_TOKEN = "Токен Тинькофф (биржа)"
     TINKOFF_SANDBOX_TOKEN = "Токен Тинькофф (песочница)"
 
+    WORKING_DIRECTORY = "Рабочая директория"
+
 
 def extract_config_field(
     configuration: Dict[str, Any],
     field: ConfigFields,
-    dtype: FIELD_TYPE = str,
+    dtype: Type[FIELD_TYPE] = str,
     default: Optional[FIELD_TYPE] = None,
 ) -> FIELD_TYPE:
     value = configuration.get(field.value)
@@ -27,20 +29,28 @@ def extract_config_field(
             raise ValueError(
                 f"Необходимо задать параметр {field} в конфигурационном файле"
             )
-    if type(value) != dtype:
-        raise TypeError(
-            f"Неверный тип данных для пераметра {field} "
-            f"в конфигурационном файле"
-        )
+    if not isinstance(value, dtype):
+        try:
+            value = dtype(value)
+        except Exception:
+            raise TypeError(
+                f"Неверный тип данных для пераметра {field} "
+                f"в конфигурационном файле"
+            )
     return value
 
 
 with open(CONFIG_PATH, "r", encoding="utf-8") as file:
     config = yaml.safe_load(file)
 
-TINKOFF_MARKET_TOKEN = extract_config_field(
+TINKOFF_MARKET_TOKEN: str = extract_config_field(
     config, ConfigFields.TINKOFF_MARKET_TOKEN
 )
-TINKOFF_SANDBOX_TOKEN = extract_config_field(
+TINKOFF_SANDBOX_TOKEN: str = extract_config_field(
     config, ConfigFields.TINKOFF_SANDBOX_TOKEN
 )
+WORKING_DIRECTORY: Path = extract_config_field(
+    config, ConfigFields.WORKING_DIRECTORY, Path, Path("userdata")
+)
+WORKING_DIRECTORY.mkdir(parents=True, exist_ok=True)
+FIGI_FILE = WORKING_DIRECTORY.joinpath("figi_file.txt")
